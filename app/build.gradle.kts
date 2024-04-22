5c52a28074e3c1eec2f6cd99df6300f0fea4020d
MiaMiaLuz <mia1998811@gmail.com>
2024-04-22 09:05:53 +0200

    Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
+import org.jetbrains.kotlin.fir.declarations.builder.buildScript
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id ("kotlin-android")
+    id ("com.google.gms.google-services")
 }
 
 android {
@@ -38,7 +42,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -46,17 +50,22 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     //Jetpack compose
-    implementation ("androidx.compose.ui:ui:1.6.5")
-    implementation ("androidx.compose.material:material:1.6.5")
-    implementation ("androidx.compose.ui:ui-tooling:1.6.5")
-    implementation ("androidx.compose.foundation:foundation:1.6.5")
+    implementation ("androidx.compose.ui:ui:1.6.6")
+    implementation ("androidx.compose.material:material:1.6.6")
+    implementation ("androidx.compose.ui:ui-tooling:1.6.6")
+    implementation ("androidx.compose.foundation:foundation:1.6.6")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     //Firebase
-    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+    implementation ("com.google.gms:google-services:4.4.1")
+    implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
     implementation("com.google.firebase:firebase-database")
-    implementation ("com.google.firebase:firebase-analytics-ktx:20.0.0")
+    implementation ("com.google.firebase:firebase-analytics-ktx:21.6.2")
+    implementation ("com.google.firebase:firebase-core:21.1.1")
     implementation("com.google.firebase:firebase-auth")
+    //Biometric
+    implementation("androidx.biometric:biometric:1.0.1")
+
 
     //implementation ("com.google.firebase:firebase-auth-ktx:21.0.0")
     //implementation ("com.google.firebase:firebase-firestore-ktx:23.0.0")
--- app/src/main/java/com/example/firemind/InicioDeSesion/InicioDeSesion.kt
+++ app/src/main/java/com/example/firemind/InicioDeSesion/InicioDeSesion.kt
@@ -1,8 +1,9 @@
 package com.example.firemind.InicioDeSesion
 
-import android.hardware.biometrics.BiometricPrompt
 import android.os.Build
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget.Button
@@ -10,16 +11,23 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricPrompt
 import androidx.core.content.ContextCompat
 import com.example.firemind.R
+import com.google.firebase.FirebaseApp
 import java.util.Calendar
 import com.google.firebase.auth.FirebaseAuth
 
-class InicioDeSesion : AppCompatActivity(), OnClickListener {
+class InicioDeSesion : AppCompatActivity(), OnClickListener, TextWatcher {
 
     private lateinit var editTextEmail : EditText
     private lateinit var editTextPassword : EditText
     private lateinit var buttonCrearCuenta : Button
+    private lateinit var buttonInicioDeSesion : Button
+    private lateinit var biometria : Button
+    private lateinit var auth: FirebaseAuth
+    private var canAuth : Boolean = false
+    private lateinit var prompt: BiometricPrompt.PromptInfo
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio_de_sesion)
@@ -27,9 +35,35 @@ class InicioDeSesion : AppCompatActivity(), OnClickListener {
         editTextEmail = findViewById(R.id.User)
         editTextPassword = findViewById(R.id.Password)
         buttonCrearCuenta = findViewById(R.id.CrearCuenta)
+        buttonInicioDeSesion = findViewById(R.id.InicioDeSesion)
+        biometria = findViewById(R.id.Biometria)
+
         buttonCrearCuenta.setOnClickListener(this)
+        buttonInicioDeSesion.setOnClickListener(this)
+        biometria.setOnClickListener(this)
+
+        FirebaseApp.initializeApp(this)
+        auth = FirebaseAuth.getInstance()
+        buttonCrearCuenta.addTextChangedListener(this)
+        buttonInicioDeSesion.addTextChangedListener(this)
+
+        if(androidx.biometric.BiometricManager.from(this).canAuthenticate()
+            == androidx.biometric.BiometricManager.BIOMETRIC_SUCCESS
+        ){
+            canAuth = true
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            prompt = BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Autenticacion")
+                .setSubtitle("Usa el sensor biométrico")
+                .setNegativeButtonText("Cerrar")
+                .setConfirmationRequired(false)
+                .build()
+        }
+
     }
 
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onClick(v: View?) {
         when(v?.id){
             R.id.CrearCuenta -> {
@@ -39,41 +73,46 @@ class InicioDeSesion : AppCompatActivity(), OnClickListener {
                 iniciarSesion()
             }
             R.id.Biometria -> {
-                biometria()
+                biometria(prompt)
             }
         }
     }
-    fun biometria(){
-        val executor = ContextCompat.getMainExecutor(this)
-        val callback = @RequiresApi(Build.VERSION_CODES.P)
-        object : BiometricPrompt.AuthenticationCallback() {
-            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
-                super.onAuthenticationError(errorCode, errString)
-                Toast.makeText(
-                    applicationContext,
-                    "Error de autenticación: $errString",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
+    @RequiresApi(Build.VERSION_CODES.Q)
+    fun biometria(auth: BiometricPrompt.PromptInfo){
 
-            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
-                super.onAuthenticationSucceeded(result)
-                Toast.makeText(applicationContext, "Autenticación exitosa", Toast.LENGTH_SHORT)
-                    .show()
-            }
+        if(canAuth){
+            BiometricPrompt(this, ContextCompat.getMainExecutor(this),
+                object : BiometricPrompt.AuthenticationCallback() {
+                    override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                        super.onAuthenticationError(errorCode, errString)
+                        Toast.makeText(
+                            applicationContext,
+                            "Error de autenticación: $errString",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
 
-            override fun onAuthenticationFailed() {
-                super.onAuthenticationFailed()
-                Toast.makeText(applicationContext, "Autenticación fallida", Toast.LENGTH_SHORT)
-                    .show()
-            }
+                    override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                        super.onAuthenticationSucceeded(result)
+                        var sharedp = getSharedPreferences("data", MODE_PRIVATE)
+                        var mail = sharedp.getString("email", "")
+                        Toast.makeText(applicationContext, "Autenticación exitosa", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+
+                    override fun onAuthenticationFailed() {
+                        super.onAuthenticationFailed()
+                        Toast.makeText(applicationContext, "Autenticación fallida", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+            ).authenticate(auth)
         }
     }
 
     fun iniciarSesion(){
         val email = editTextEmail.text.toString()
         val password = editTextPassword.text.toString()
-        var auth = FirebaseAuth.getInstance()
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
@@ -84,7 +123,6 @@ class InicioDeSesion : AppCompatActivity(), OnClickListener {
             }
     }
     fun crearUsuario(){
-        var auth = FirebaseAuth.getInstance()
         val email = editTextEmail.text.toString()
         val password = editTextPassword.text.toString()
         auth.createUserWithEmailAndPassword(email, password)
@@ -107,6 +145,19 @@ class InicioDeSesion : AppCompatActivity(), OnClickListener {
         val rootView = findViewById<View>(android.R.id.content)
         val drawableResourceId = if (isDayTime) dia else noche
         rootView.setBackgroundResource(drawableResourceId)
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+        buttonCrearCuenta.isEnabled = editTextEmail.text.isNotEmpty() && editTextPassword.text.isNotEmpty()
+        buttonInicioDeSesion.isEnabled = editTextEmail.text.isNotEmpty() && editTextPassword.text.isNotEmpty()
     }
 
 }
